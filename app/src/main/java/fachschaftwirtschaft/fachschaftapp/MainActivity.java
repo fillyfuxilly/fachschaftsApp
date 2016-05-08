ad81bad20af3f190da8f67b3a9173c1b58c2f3d7
RitterVomNi <heinen.m@web.de>
2016-05-08 15:35:06 +0200

    added Infos intent

--- app/src/main/java/fachschaftwirtschaft/fachschaftapp/MainActivity.java
+++ app/src/main/java/fachschaftwirtschaft/fachschaftapp/MainActivity.java
@@ -1,3 +1,6 @@
+/**
+ * @Author Matthias Heinen
+ */
 package fachschaftwirtschaft.fachschaftapp;
 
 import android.content.Intent;
@@ -5,10 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
+    ImageButton ibtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, TermineActivity.class));
+            }
+        });
+
+        ibtn = (ImageButton)findViewById(R.id.imageButton_infos);
+
+        ibtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, InfosActivity.class));
             }
         });
     }
