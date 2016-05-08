a42a443c4cc2c37a6ab5268cac00d1ed9ad516de
RitterVomNi <heinen.m@web.de>
2016-05-08 21:47:31 +0200

    forced register

--- app/src/main/java/fachschaftwirtschaft/fachschaftapp/RegisterActivity.java
+++ app/src/main/java/fachschaftwirtschaft/fachschaftapp/RegisterActivity.java
@@ -1,12 +1,15 @@
 package fachschaftwirtschaft.fachschaftapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -14,6 +17,7 @@ public class RegisterActivity extends AppCompatActivity {
     EditText ed1,ed2;
     Button b1;
     SharedPreferences sharedpreferences;
+    ImageButton ibtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +27,14 @@ public class RegisterActivity extends AppCompatActivity {
         ed1=(EditText)findViewById(R.id.editText);
         ed2=(EditText)findViewById(R.id.editText2);
 
-        b1=(Button)findViewById(R.id.button_register);
-        sharedpreferences = getSharedPreferences("Registrierung", Context.MODE_PRIVATE);
+        b1=(Button)findViewById(R.id.button_r);
+
+
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sharedpreferences = getSharedPreferences("Registrierung", Context.MODE_PRIVATE);
                 String n  = ed1.getText().toString();
                 String g  = ed2.getText().toString();
 
@@ -36,10 +42,20 @@ public class RegisterActivity extends AppCompatActivity {
 
                 editor.putString("nameKey", n);
                 editor.putString("gruppeKey", g);
-                editor.commit();
-                Toast.makeText(RegisterActivity.this,"Thanks", Toast.LENGTH_LONG).show();
+                editor.apply();
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                Toast.makeText(RegisterActivity.this,"Erfolgreich registriert", Toast.LENGTH_LONG).show();
 
             }
         });
+
+        /*ibtn = (ImageButton) findViewById(R.id.imageButton_back);
+
+        ibtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            }
+        });*/
     }
 }
