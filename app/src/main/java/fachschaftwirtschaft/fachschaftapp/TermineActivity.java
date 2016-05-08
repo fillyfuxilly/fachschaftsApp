1290ad074f8db54512bd7f27aa72a7f72fba528a
RitterVomNi <heinen.m@web.de>
2016-05-08 14:46:05 +0200

    Initial commit

---
+++ app/src/main/java/fachschaftwirtschaft/fachschaftapp/TermineActivity.java
@@ -0,0 +1,13 @@
+package fachschaftwirtschaft.fachschaftapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TermineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_termine);
+    }
+}
