72ab37c5bbea88b809771f8f1309fd8d78e1d01e
RitterVomNi <heinen.m@web.de>
2016-05-08 21:47:31 +0200

    forced register

--- app/src/main/java/fachschaftwirtschaft/fachschaftapp/InfosActivity.java
+++ app/src/main/java/fachschaftwirtschaft/fachschaftapp/InfosActivity.java
@@ -1,13 +1,25 @@
 package fachschaftwirtschaft.fachschaftapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class InfosActivity extends AppCompatActivity {
 
+    SharedPreferences sharedpreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infos);
+
+        sharedpreferences = getSharedPreferences("Registrierung", Context.MODE_PRIVATE);
+
+        EditText name = (EditText) findViewById(R.id.editText3);
+        name.setText(sharedpreferences.getString("nameKey", ""));
+
+        EditText gruppe = (EditText) findViewById(R.id.editText4);
+        gruppe.setText(sharedpreferences.getString("gruppeKey", ""));
     }
 }
--- app/src/main/java/fachschaftwirtschaft/fachschaftapp/MainActivity.java
+++ app/src/main/java/fachschaftwirtschaft/fachschaftapp/MainActivity.java
@@ -3,49 +3,92 @@
  */
 package fachschaftwirtschaft.fachschaftapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
     ImageButton ibtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        class RegisteredTask extends AsyncTask<Void,Void,Boolean> {
 
-        btn = (Button)findViewById(R.id.button_termine);
-
-        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, TermineActivity.class));
+            protected Boolean doInBackground(Void... params){
+                SharedPreferences sharedpreferences = getSharedPreferences("Registrierung", Context.MODE_PRIVATE);
+                String name = sharedpreferences.getString("nameKey", "");
+                Boolean result = false;
+                if(name.equals(""))return result;
+
+                else {
+                    result = true;
+                    return result;
+                }
+
             }
-        });
-
-        btn = (Button)findViewById(R.id.button_gr);
-
-        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
-            }
-        });
+            protected void onPostExecute(Boolean result) {
+                if(result) {
+                    setContentView(R.layout.activity_main);
 
-        ibtn = (ImageButton)findViewById(R.id.imageButton_infos);
+                    btn = (Button) findViewById(R.id.button_termine);
 
-        ibtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, InfosActivity.class));
+                    btn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            startActivity(new Intent(MainActivity.this, TermineActivity.class));
+                        }
+                    });
+
+                    btn = (Button) findViewById(R.id.button_gr);
+
+                    btn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                        }
+                    });
+
+                    btn = (Button) findViewById(R.id.button_clear);
+
+                    btn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            SharedPreferences sharedpreferences = getSharedPreferences("Registrierung", Context.MODE_PRIVATE);
+                            SharedPreferences.Editor e = sharedpreferences.edit();
+                            e.clear();
+                            e.apply();
+                            Toast.makeText(MainActivity.this,"Daten gelöscht", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                    ibtn = (ImageButton) findViewById(R.id.imageButton_infos);
+
+                    ibtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            startActivity(new Intent(MainActivity.this, InfosActivity.class));
+                        }
+                    });
+                } else startActivity(new Intent(MainActivity.this, RegisterActivity.class));
             }
-        });
+
+
+        }new RegisteredTask().execute();
+
     }
+
 
 
 }
