be384d64c3fcaac78c13688a4d7601a69eed435b
RitterVomNi <heinen.m@web.de>
2016-05-08 15:35:06 +0200

    added Infos intent

---
+++ app/src/main/java/fachschaftwirtschaft/fachschaftapp/InfosActivity.java
@@ -0,0 +1,13 @@
+package fachschaftwirtschaft.fachschaftapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infos);
+    }
+}
