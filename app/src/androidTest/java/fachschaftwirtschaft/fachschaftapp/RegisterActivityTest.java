8d3a7502c49574ac0dc1c5cc44031b7f36757ea1
RitterVomNi <heinen.m@web.de>
2016-06-05 01:07:12 +0200

    tried to fix register test at least :(

--- app/src/androidTest/java/fachschaftwirtschaft/fachschaftapp/RegisterActivityTest.java
+++ app/src/androidTest/java/fachschaftwirtschaft/fachschaftapp/RegisterActivityTest.java
@@ -1,8 +1,8 @@
 package fachschaftwirtschaft.fachschaftapp;
 
-import android.app.Activity;
-import android.app.Instrumentation;
+
 import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -14,23 +14,12 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import android.content.Intent;
-import android.test.InstrumentationTestCase;
+import static android.support.test.espresso.action.ViewActions.*;
 
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.Intents.intending;
-import static android.support.test.espresso.intent.Intents.times;
-import android.support.test.rule.ActivityTestRule;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
@@ -42,36 +31,40 @@ import static org.hamcrest.Matchers.instanceOf;
  */
 @RunWith(AndroidJUnit4.class)
 public class RegisterActivityTest {
-
+RegisterActivity activity;
 
     @Rule
     public ActivityTestRule<RegisterActivity> rule = new ActivityTestRule<>(RegisterActivity.class, true, false);
 
-
-    @Test
-    public void registerClick() {
+    @Before
+    public void setUp() {
         Intents.init();
-        rule.launchActivity(new Intent());
-        onView(withId(R.id.editText)).perform(typeText("MatThias"), closeSoftKeyboard());
-        onView(withId(R.id.spinner)).perform(click());
-        onData(allOf(is(instanceOf(String.class)), is("3"))).perform(click());
-        onView(withId(R.id.button_r)).perform(click());
-        intending(toPackage(MainActivity.class.getName()));
+        activity = rule.launchActivity(null);
+    }
+    @After
+    public void tearDown() {
         Intents.release();
     }
 
     @Test
+    public void registerClick() {
+        onView(ViewMatchers.withId(R.id.editText)).perform(typeText("MatThias"), closeSoftKeyboard());
+        onView(ViewMatchers.withId(R.id.spinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("3"))).perform(click());
+        onView(ViewMatchers.withId(R.id.button_r)).perform(click());
+        intending(toPackage(MainActivity.class.getName()));
+    }
+
+    @Test
     public void typeName() {
-        rule.launchActivity(new Intent());
-        onView(withId(R.id.editText)).perform(typeText("MatThias"), closeSoftKeyboard());
-        onView(withId(R.id.editText)).check(matches(withText("MatThias")));
+        onView(ViewMatchers.withId(R.id.editText)).perform(typeText("MatThias"), closeSoftKeyboard());
+        onView(ViewMatchers.withId(R.id.editText)).check(matches(withText("MatThias")));
     }
     @Test
     public void selectGroup() {
-        rule.launchActivity(new Intent());
-        onView(withId(R.id.spinner)).perform(click());
+        onView(ViewMatchers.withId(R.id.spinner)).perform(click());
         onData(allOf(is(instanceOf(String.class)), is("3"))).perform(click());
-        onView(withId(R.id.spinner)).check(matches(withSpinnerText(containsString("3"))));
+        onView(ViewMatchers.withId(R.id.spinner)).check(matches(withSpinnerText(containsString("3"))));
     }
 
 }
