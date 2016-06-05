acb61eea300c1e1b2bdb8d31b08ff1c799e1e38f
RitterVomNi <heinen.m@web.de>
2016-06-05 23:57:58 +0200

    added logging

--- app/src/main/java/fachschaftwirtschaft/fachschaftapp/MainActivity.java
+++ app/src/main/java/fachschaftwirtschaft/fachschaftapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn;
     ImageButton ibtn;
+    private static final String TAG = "MainActivity";
 
 
     @Override
@@ -31,61 +33,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(TAG, "Layout geladen");
 
 
-        btn = (Button) findViewById(R.id.button_clear);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences sharedpreferences = getSharedPreferences("Registrierung", Context.MODE_PRIVATE);
-                SharedPreferences.Editor e = sharedpreferences.edit();
-                e.clear();
-                e.apply();
-                Toast.makeText(MainActivity.this,"Daten gelöscht", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        ibtn = (ImageButton) findViewById(R.id.imageButton_infos);
-
-        ibtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, InfosActivity.class));
-            }
-        });
-
+        /**
+         * AsnyTask der prüft, ob der User sich bereits registriert hat. Falls ja wird MainActivity zu Ende aufgebaut.
+         * Falls nein, dann wird der RegisterActivity Dialog gestartet.
+         */
         class RegisteredTask extends AsyncTask<Void,Void,Boolean> {
 
             @Override
             protected Boolean doInBackground(Void... params){
+
                 SharedPreferences sharedpreferences = getSharedPreferences("Registrierung", Context.MODE_PRIVATE);
                 String name = sharedpreferences.getString("nameKey", "");
-                Boolean result = false;
-                if(name.equals(""))return result;
-                else {
-                    result = true;
-                    return result;
-                }
+
+               return name.equals("");
+
             }
+
             @Override
             protected void onPostExecute(Boolean result) {
-                if(result) {
-/*                    WebView displayYoutubeVideo = (WebView) findViewById(R.id.webView);
-                    String frameVideo = "<iframe width=\"370\" height=\"290\" src=\"https://www.youtube.com/embed/ExG9CydpZO4\" frameborder=\"0\" allowfullscreen></iframe>";
 
-                    displayYoutubeVideo.setVisibility(View.VISIBLE);
-                    displayYoutubeVideo.setWebViewClient(new WebViewClient() {
+                if(result) {
+
+                    Log.d(TAG, "User bereits registriert");
+
+                    btn = (Button) findViewById(R.id.button_clear);
+
+                    btn.setOnClickListener(new View.OnClickListener() {
+
+
                         @Override
-                        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                            return false;
+                        public void onClick(View v) {
+                            SharedPreferences sharedpreferences = getSharedPreferences("Registrierung", Context.MODE_PRIVATE);
+                            SharedPreferences.Editor e = sharedpreferences.edit();
+                            e.clear();
+                            e.apply();
+
+                            Toast.makeText(MainActivity.this,"Daten gelöscht", Toast.LENGTH_LONG).show();
+
                         }
                     });
-                    WebSettings webSettings = displayYoutubeVideo.getSettings();
-                    webSettings.setJavaScriptEnabled(true);
-                    displayYoutubeVideo.loadData(frameVideo, "text/html", "utf-8");
-*/
-                } else startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+
+                    ibtn = (ImageButton) findViewById(R.id.imageButton_infos);
+
+                    ibtn.setOnClickListener(new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View v) {
+                            startActivity(new Intent(MainActivity.this, InfosActivity.class));
+
+                        }
+                    });
+                } else {
+
+                    Log.d(TAG, "User nicht registriert, starte RegisterActivity");
+
+                    startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                }
             }
         }new RegisteredTask().execute();
     }
@@ -108,8 +114,16 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Startet TermineActivity
+     * @param button, der mit android:onClick im xml Layout eingebunden ist
+     */
     public void startTermine (View button) {
+
+        Log.d(TAG, "Termine Button gedrückt");
+
         startActivity(new Intent(MainActivity.this, TermineActivity.class));
+
     }
 
     /**
@@ -117,13 +131,22 @@ public class MainActivity extends AppCompatActivity {
      * @param button, der mit android:onClick im xml Layout eingebunden ist
      */
     public void startVideo (View button) {
+
             try {
+
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + "ExG9CydpZO4"));
                 startActivity(intent);
+
+                Log.d(TAG, "Youtube App gestartet");
+
             } catch (ActivityNotFoundException ex) {
+
                 Intent intent = new Intent(Intent.ACTION_VIEW,
                         Uri.parse("http://www.youtube.com/watch?v=" + "ExG9CydpZO4"));
                 startActivity(intent);
+
+                Log.d(TAG, "Keine Youtube App vorhanden");
+
             }
     }
 }
