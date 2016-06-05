198865526fc5cb33d8443d5c858860fcade1929f
RitterVomNi <heinen.m@web.de>
2016-06-05 12:43:17 +0200

    changed some RegisterTests

--- app/src/androidTest/java/fachschaftwirtschaft/fachschaftapp/RegisterActivityTest.java
+++ app/src/androidTest/java/fachschaftwirtschaft/fachschaftapp/RegisterActivityTest.java
@@ -9,8 +9,6 @@ import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
@@ -37,37 +35,36 @@ import static org.hamcrest.Matchers.instanceOf;
 @RunWith(AndroidJUnit4.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class RegisterActivityTest {
-RegisterActivity activity;
 
     @Rule
     public ActivityTestRule<RegisterActivity> rule = new ActivityTestRule<>(RegisterActivity.class);
 
 
     @Test
-    public void test1TypeName() {
-        onView(ViewMatchers.withId(R.id.editText)).perform(typeText("MatThias"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.editText)).check(matches(withText("MatThias")));
+    public void test1TypeName() throws Exception {
+
+        onView(withId(R.id.editText)).perform(click());
+        onView(withId(R.id.editText)).perform(replaceText("MatThias"));
+        onView(withId(R.id.editText)).check(matches(withText("MatThias")));
     }
     @Test
-    public void test2SelectGroup() {
+    public void test2SelectGroup()throws Exception {
         onView(ViewMatchers.withId(R.id.spinner)).perform(click());
         onData(allOf(is(instanceOf(String.class)), is("3"))).perform(click());
         onView(ViewMatchers.withId(R.id.spinner)).check(matches(withSpinnerText(containsString("3"))));
     }
 
     @Test
-    public void test3RegisterClick() {
-        Intents.init();
+    public void test3RegisterClick()throws Exception {
         onView(ViewMatchers.withId(R.id.editText)).perform(typeText("MatThias"), closeSoftKeyboard());
         onView(ViewMatchers.withId(R.id.spinner)).perform(click());
         onData(allOf(is(instanceOf(String.class)), is("3"))).perform(click());
         onView(ViewMatchers.withId(R.id.button_r)).perform(click());
-        intending(toPackage(MainActivity.class.getName()));
-        Intents.release();
+        onView(withId(R.id.spinner)).check(doesNotExist());
     }
 
     @Test
-    public void test4ClearSharedPrefs() {
+    public void test4ClearSharedPrefs()throws Exception {
         Activity activity = rule.getActivity();
         SharedPreferences prefs = activity.getSharedPreferences("Registrierung", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
