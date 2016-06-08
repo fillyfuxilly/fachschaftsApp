f3294de2357ccc1cb2cf8e2a7386a5fd7a13f82c
Wendy <wendy@frevert.de>
2016-06-08 20:14:07 +0200

    small changes info activity and layout

--- app/src/main/java/fachschaftwirtschaft/fachschaftapp/InfosActivity.java
+++ app/src/main/java/fachschaftwirtschaft/fachschaftapp/InfosActivity.java
@@ -1,19 +1,30 @@
 package fachschaftwirtschaft.fachschaftapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class InfosActivity extends AppCompatActivity {
 
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infos);
 
-
+        // zurueck zur Startseite
+        tv = (TextView) findViewById(R.id.zurueck);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InfosActivity.this, MainActivity.class));
+            }
+        });
     }
 }
