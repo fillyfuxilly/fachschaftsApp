e922e86d9b8185c05a36ba55f1f437dbe77a8aea
Wendy <wendy@frevert.de>
2016-06-09 23:20:09 +0200

    minor change in termine activity and layout

--- app/src/main/java/fachschaftwirtschaft/fachschaftapp/TermineActivity.java
+++ app/src/main/java/fachschaftwirtschaft/fachschaftapp/TermineActivity.java
@@ -1,13 +1,28 @@
 package fachschaftwirtschaft.fachschaftapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class TermineActivity extends AppCompatActivity {
+
+    TextView tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_termine);
+
+        // zurueck zur Startseite
+        tv2 = (TextView) findViewById(R.id.zurueck);
+        tv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TermineActivity.this, MainActivity.class));
+            }
+        });
     }
+
 }
